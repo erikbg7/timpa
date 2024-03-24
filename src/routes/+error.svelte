81c25aba70d99bd38e0c6bd14039ab61fcf4c641
Erik <erik.bg7@gmail.com>
2024-03-24 21:58:24 +0100

    Add 404 error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,13 +1,17 @@
-<html lang="en">
-	<head>
-		<meta charset="utf-8" />
-		<title>%sveltekit.error.message%</title>
-	</head>
-	<body>
-		<div class="absolute inset-0 bg-red-300">
-			<h1>My custom error page s87df ys87 f7s7df7s 7f7s 7df7s d7f7df</h1>
-			<p>Status: %sveltekit.status%</p>
-			<p>Message: %sveltekit.error.message%</p>
-		</div>
-	</body>
-</html>
+<script>
+	// Aka 404 error page
+	import Seo from '$lib/components/SEO.svelte';
+</script>
+
+<Seo />
+<main>
+	<div class="flex h-screen w-screen flex-col items-center justify-center">
+		<img alt="page not found" src="/img/404.svg" class="max-w-sm" />
+		<h2 class="mb-8 text-4xl font-semibold text-neutral-200">Page not found</h2>
+		<p class="mb-8 max-w-sm text-center md:max-w-lg">
+			We're sorry, but the page you requested could not be found. This could be because the page has
+			been moved or deleted. We're tracking these errors so we can fix them in the future.
+		</p>
+		<a href="/" class="btn btn-secondary btn-sm">Back home</a>
+	</div>
+</main>
